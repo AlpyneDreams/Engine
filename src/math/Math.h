e3514854e1f8a1fa4cdea31e81c9fa19f1a748d0
Alpyne <alpyne.dreams@gmail.com>
2022-04-01 08:45:43 -0700

    Add common math class typedefs

--- src/math/Math.h
+++ src/math/Math.h
@@ -2,3 +2,13 @@
 
 #define HLSLPP_FEATURE_TRANSFORM
 #include "hlsl++.h"
+
+namespace engine
+{
+    using Vector2    = hlslpp::float2;
+    using Vector3    = hlslpp::float3;
+    using Vector4    = hlslpp::float4;
+    using Quaternion = hlslpp::quaternion;
+
+    using Matrix4x4  = hlslpp::float4x4;
+}
