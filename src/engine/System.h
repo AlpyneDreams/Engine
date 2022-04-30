4a2115d61fe1eaee36374a9488ecd54b4bee971c
Alpyne <alpyne.dreams@gmail.com>
2022-04-30 16:37:49 -0700

    SystemGroup: Use VTable reflection

--- src/engine/System.h
+++ src/engine/System.h
@@ -8,6 +8,7 @@
 #include <type_traits>
 
 #include "common/Ranges.h"
+#include "common/VTable.h"
 
 // TODO: Move this into /entity/? or keep in /engine/? 
 
@@ -20,6 +21,17 @@ namespace engine
         virtual void Tick() {}
 
         virtual ~System() {}
+    };
+
+    using SystemFunc = void(System*);
+
+    template <>
+    struct VTable<System>
+    {
+        SystemFunc* Start;
+        SystemFunc* Update;
+        SystemFunc* Tick;
+        void*       Destructor;
     };
 
     struct Behavior;
@@ -41,48 +53,84 @@ namespace engine
         bool started = false;
 
         // TODO: non-owned systems?
+        // TODO: system update order
+        // TODO: VTable checks for Behaviors
 
         std::multimap<std::type_index, std::shared_ptr<System>> systems;
+        std::map<System*, SystemFunc*> OnStart, OnUpdate, OnTick;
 
     public:
         // Can be destructured to a std::pair<std::type_index, std::shared_ptr<System>>
         using Iterator   = decltype(systems)::const_iterator;
         using SystemList = IteratorRange<Iterator>;
-    
-        template <SystemClass Sys>
-        SystemList GetSystems() const {
-            return systems.equal_range(typeid(Sys));
-        }
 
+        SystemGroup() {}
+        SystemGroup(auto*... sys) {
+            (systems.insert({typeid(decltype(sys)), std::shared_ptr<System>(sys)}), ...);
+        }
+    
         template <SystemClass Sys>
         Sys& AddSystem(auto&... args)
         {
+            static System nullsystem;
+            static VTable<System>& base = GetVTable(&nullsystem);
+
             auto system = std::make_shared<Sys>(args...);
-            systems.insert({ typeid(Sys), system  });
-                
-            // If Start() has already been called, then call
-            // it on new systems as soon as they're created.
-            if (started) {
-                system->Start();
+            auto* sys   = system.get();
+
+            systems.insert({typeid(Sys), system});
+
+            VTable<System>& vt = GetVTable<System>(sys);
+
+            if (vt.Start != base.Start) {
+                OnStart.insert({sys, vt.Start});
+
+                // If Start() has already been called, then call
+                // it on new systems as soon as they're created.
+                if (started) {
+                    system->Start();
+                }
             }
 
-            return *system.get();
+            if (vt.Update != base.Update) {
+                OnUpdate.insert({sys, vt.Update});
+            }
+
+            if (vt.Tick != base.Tick) {
+                OnTick.insert({sys, vt.Tick});
+            }
+
+            return *sys;
+        }
+
+        template <SystemClass Sys>
+        SystemList GetSystems() const {
+            return GetSystems(typeid(Sys));
+        }
+
+        SystemList GetSystems(std::type_index type) const {
+            return systems.equal_range(type);
         }
 
         // Remove all systems of type Sys
         template <SystemClass Sys>
         void RemoveSystems() {
-            systems.erase(typeid(Sys));
+            RemoveSystems(typeid(Sys));
         }
 
         // Remove all systems of 'type'
         void RemoveSystems(std::type_index type) {
+            for (auto& [t, sys] : GetSystems(type)) {
+                auto* ptr = sys.get();
+                UnregisterCallbacks(ptr);
+            }
             systems.erase(type);
         }
 
         // Removes a single system
         void RemoveSystem(Iterator iterator) {
             systems.erase(iterator);
+            UnregisterCallbacks(iterator->second.get());
         }
 
     // Iteration:
@@ -92,17 +140,20 @@ namespace engine
     
     // System overrides:
 
-        void Start() final override {
-            started = true;
-            for (auto& [t, sys] : systems) {sys->Start();}
+        void Start() final override { started = true; Call(OnStart); }
+        void Update() final override { Call(OnUpdate); }
+        void Tick() final override { Call(OnTick); }
+
+    protected:
+        inline void Call(auto& event) {
+            for (auto& [sys, Func] : event) { Func(sys); }
         }
 
-        void Update() final override {
-            for (auto& [t, sys] : systems) {sys->Update();}
-        }
-
-        void Tick() final override {
-            for (auto& [t, sys] : systems) {sys->Tick();}
+        inline void UnregisterCallbacks(System* ptr)
+        {
+            OnStart.erase(ptr);
+            OnUpdate.erase(ptr);
+            OnTick.erase(ptr);
         }
     };
 }
