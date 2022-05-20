9553b332507b0172d4fece9ae8c6c0cf80133a93
Alpyne <alpyne.dreams@gmail.com>
2022-05-19 21:19:17 -0700

    Add BGFX as submodule

--- src/platform/Platform.h
+++ src/platform/Platform.h
@@ -12,12 +12,3 @@
 #define PLATFORM_SDL     1
 #define PLATFORM_BGFX    1
 
-// HACK: Manually set BX_CONFIG_DEBUG so we can use bx lib,
-// since the CMake scripts are wrongly defining it as empty.
-// Include Platform.h to be able to use <bx/*.h>
-#ifdef PLATFORM_BGFX
-    #if defined(BX_CONFIG_DEBUG)
-        #undef BX_CONFIG_DEBUG
-        #define BX_CONFIG_DEBUG 0
-    #endif
-#endif
