b824d932bad8db26d267c898701054241814134c
Alpyne <alpyne.dreams@gmail.com>
2022-06-20 17:34:20 -0700

    Allow resizing ImGui windows

--- src/imgui/Window.h
+++ src/imgui/Window.h
@@ -24,7 +24,7 @@ namespace engine::GUI
             if (!open)
                 return;
             
-            ImGui::SetNextWindowSize(ImVec2(float(width), float(height)));
+            ImGui::SetNextWindowSize(ImVec2(float(width), float(height)), ImGuiCond_FirstUseEver);
             if (!ImGui::Begin(name, &open, flags))
             {
                 return ImGui::End();
