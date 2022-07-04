2958579378be440cfb8388880ae89e89f6585db9
Alpyne <alpyne.dreams@gmail.com>
2022-07-04 16:39:31 +0100

    Allow decomposing GLM vectors

--- src/math/Math.h
+++ src/math/Math.h
@@ -65,8 +65,31 @@ namespace engine
 
 }
 
+namespace std
+{
+    template <auto N, class T, auto Q>
+    struct tuple_size<glm::vec<N, T, Q>> : std::integral_constant<std::size_t, N> { };
+
+    template <std::size_t I, auto N, class T, auto Q>
+    struct tuple_element<I, glm::vec<N, T, Q>> {
+        using type = decltype(get<I>(declval<glm::vec<N,T,Q>>()));
+    };
+}
+
 namespace glm
 {
+    template <std::size_t I, auto N, class T, auto Q>
+    constexpr auto& get(glm::vec<N, T, Q>& v) noexcept { return v[I]; }
+
+    template <std::size_t I, auto N, class T, auto Q>
+    constexpr const auto& get(const glm::vec<N, T, Q>& v) noexcept { return v[I]; }
+
+    template <std::size_t I, auto N, class T, auto Q>
+    constexpr auto&& get(glm::vec<N, T, Q>&& v) noexcept { return std::move(v[I]); }
+
+    template <std::size_t I, auto N, class T, auto Q>
+    constexpr const auto&& get(const glm::vec<N, T, Q>&& v) noexcept { return std::move(v[I]); }
+
     // Automatic GLM string casts.
     // fmtlib requires these be in namespace glm
 
