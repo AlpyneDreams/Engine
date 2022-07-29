8b0b5f14929f9c3652e1e5c2f194d8f2e33577e5
Alpyne <alpyne.dreams@gmail.com>
2022-07-28 21:18:52 -0700

    Add placeholder Blinn-Phong shading

--- shaders/vs_cubes.glsl
+++ shaders/vs_cubes.glsl
@@ -1,10 +1,14 @@
 $input a_position, a_normal
-$output v_normal
+$output v_normal, v_view
 
 #include "bgfx_shader.sh"
 
 void main()
 {
-	gl_Position = mul(u_modelViewProj, vec4(a_position, 1.0) );
-	v_normal = a_normal;
+	vec3 pos = a_position;
+	vec3 normal = a_normal.xyz*2.0 - 1.0;
+
+	gl_Position = mul(u_modelViewProj, vec4(pos, 1));
+	v_normal = mul(u_model[0], vec4(normal, 0)).xyz;
+	v_view = mul(u_modelView, vec4(pos, 1)).xyz;
 }
